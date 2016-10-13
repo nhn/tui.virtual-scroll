a0c9b555f2e4a02a0646639f7fbd0c825535bc7a
강지웅/FE개발팀/NE <jiung.kang@nhnent.com>
2016-10-13 18:44:21 +0900

    feat: Add virtual scroll component

---
+++ src/js/virtualScroll.js
@@ -0,0 +1,380 @@
+/**
+ * @fileoverview Virtual scroll component.
+ * @author NHN Ent.
+ *         FE Development Lab <dl_javascript@nhnent.com>
+ */
+
+'use strict';
+
+var eventListener = require('./eventListener');
+
+var CSS_CLASS_LAYOUT = 'tui-component-virtual-scroll';
+var CSS_CLASS_ITEM_WRAPPER = CSS_CLASS_LAYOUT + '-wrapper';
+var CSS_CLASS_ITEM = CSS_CLASS_LAYOUT + '-item';
+var DEFAULT_CONTENT_HEIGHT = 50;
+var DEFAULT_DISPLAY_COUNT = 10;
+var DEFAULT_SPARE_ITEM_COUNT = 5;
+var PUBLIC_EVENT_MAP = {
+    scroll: 'scroll',
+    scrollTop: 'scrollTop',
+    scrollBottom: 'scrollBottom'
+};
+
+var VirtualScroll;
+
+require('../css/style.css');
+
+VirtualScroll = tui.util.defineClass(/** @lends VirtualScroll.prototype */{
+    /**
+     * Virtual scroll component.
+     * @constructs VirtualScroll
+     * @param {HTMLElement|String} container - container element or id
+     * @param {object} options - virtual scroll component  options
+     *      @param {?Array.<String>} options.items - items
+     *      @param {?Number} options.spareItemCount - spare item count for rendering margin of wrapper area
+     *      @param {?Number} options.itemHeight - item height
+     *      @param {?Number} options.displayCount - display item count
+     *      @param {?Number} options.layoutHeight - layout height
+     *      @param {?Number} options.scrollPosition - scroll position
+     */
+    init: function(container, options) {
+        options = options || {};
+        options.scrollPosition = options.scrollPosition || 0;
+
+        this._initData(options);
+
+        /**
+         * container element
+         * @type {HTMLElement}
+         */
+        this.container = tui.util.isString(container) ? document.getElementById(container) : container;
+
+        /**
+         * layout element
+         * @type {HTMLElement}
+         */
+        this.layout = this._renderLayout(this.container);
+
+        this._renderContents(options.scrollPosition);
+        this._attachEvent();
+    },
+
+    /**
+     * Update index range.
+     * @param {Number} scrollPosition - scrollPosition for scroll
+     * @private
+     */
+    _updateIndexRange: function(scrollPosition) {
+        var maximumEndIndex = this.items.length;
+        var realStartIndex;
+
+        realStartIndex = parseInt(scrollPosition / this.itemHeight, 0);
+
+        /**
+         * start index for picking item from total items, when rendering contents
+         * @type {Number}
+         */
+        this.startIndex = Math.max(realStartIndex - this.spareItemCount, 0);
+
+        /**
+         * end index for picking item from total items, when rendering contents
+         * @type {Number}
+         */
+        this.endIndex = Math.min(realStartIndex + this.displayCount + this.spareItemCount, maximumEndIndex);
+    },
+
+    /**
+     * Set display count.
+     * @param {?Number} displayCount - displayCount option
+     * @param {?Number} layoutHeight - layerHeight option
+     * @private
+     */
+    _setDisplayCount: function(displayCount, layoutHeight) {
+        var remainHeight;
+
+        /**
+         * display item count.
+         * @type {Number}
+         */
+        if (!displayCount && layoutHeight) {
+            remainHeight = layoutHeight % this.itemHeight;
+            this.displayCount = parseInt(layoutHeight / this.itemHeight, 0) + (remainHeight ? 1 : 0);
+        } else {
+            this.displayCount = displayCount || DEFAULT_DISPLAY_COUNT;
+        }
+    },
+
+    /**
+     * Initialize data.
+     * @param {object} options - virtual scroll component options
+     *      @param {?Array.<String>} options.items - items
+     *      @param {?Number} options.spareItemCount - spare item count for rendering margin of wrapper area
+     *      @param {?Number} options.itemHeight - item height
+     *      @param {?Number} options.displayCount - display item count
+     *      @param {?Number} options.layoutHeight - layout height
+     *      @param {?Number} options.scrollPosition - scroll position
+     * @private
+     */
+    _initData: function(options) {
+        var spareItemCount = options.spareItemCount;
+
+        /**
+         * items for rendering contents.
+         * @type {Array.<String>}
+         */
+        this.items = options.items || [];
+
+        /**
+         * spare item count for rendering margin of wrapper area
+         * @type {Number}
+         */
+        this.spareItemCount = tui.util.isExisty(spareItemCount) ? spareItemCount : DEFAULT_SPARE_ITEM_COUNT;
+
+        /**
+         * item height for rendering item.
+         * @type {Number}
+         */
+        this.itemHeight = options.itemHeight || DEFAULT_CONTENT_HEIGHT;
+        this._setDisplayCount(options.displayCount, options.layoutHeight);
+
+        /**
+         * layout height for rendering layout
+         * @type {Number}
+         */
+        this.layoutHeight = options.layoutHeight || (this.itemHeight * this.displayCount);
+
+        this._updateIndexRange(options.scrollPosition);
+    },
+
+    /**
+     * Render layout.
+     * @param {HTMLElement} container - container element
+     * @returns {HTMLElement}
+     * @private
+     */
+    _renderLayout: function(container) {
+        var layout;
+
+        if (!container) {
+            throw new Error('Not exist HTML container');
+        }
+
+        if (!tui.util.isHTMLTag(container)) {
+            throw new Error('This container is not HTML element');
+        }
+
+        layout = document.createElement('DIV');
+        layout.className = CSS_CLASS_LAYOUT;
+        layout.style.height = this.layoutHeight + 'px';
+        container.appendChild(layout);
+
+        return layout;
+    },
+
+    /**
+     * Create cssText.
+     * @param {Object.<String, Number>} cssMap - css map
+     * @returns {String}
+     * @private
+     */
+    _createCssText: function(cssMap) {
+        return tui.util.map(cssMap, function(value, property) {
+            return property + ':' + value + 'px';
+        }).join(';');
+    },
+
+    /**
+     * Create div html.
+     * @param {Object.<String, String>} attrMap - attribute map
+     * @param {String} innerHtml - html string
+     * @returns {String}
+     * @private
+     */
+    _createDivHtml: function(attrMap, innerHtml) {
+        var attrString = tui.util.map(attrMap, function(value, property) {
+            return property + '="' + value + '"';
+        }).join(' ');
+
+        return '<div ' + attrString + '>' + innerHtml + '</div>';
+    },
+
+    /**
+     * Create items html.
+     * @param {Array.<String>} items - items
+     * @returns {String}
+     * @private
+     */
+    _createItemsHtml: function(items) {
+        var itemCssText = this._createCssText({
+            height: this.itemHeight
+        });
+
+        return tui.util.map(items, function(item) {
+            return this._createDivHtml({
+                'class': CSS_CLASS_ITEM,
+                style: itemCssText
+            }, item);
+        }, this).join('');
+    },
+
+    /**
+     * Create cssText for item wrapper element.
+     * @param {Number} renderingItemCount - item count for rendering
+     * @param {Number} itemCount - total item count
+     * @returns {String}
+     * @private
+     */
+    _createItemWrapperCssText: function(renderingItemCount, itemCount) {
+        var itemHeight = this.itemHeight;
+        var height = itemHeight * renderingItemCount;
+        var marginTop = this.startIndex * itemHeight;
+        var marginBottom = (itemCount - this.endIndex) * itemHeight;
+
+        return this._createCssText({
+            height: height,
+            'margin-top': marginTop,
+            'margin-bottom': marginBottom
+        });
+    },
+
+    /**
+     * Create html for item wrapper element
+     * @returns {String}
+     * @private
+     */
+    _createItemWrapperHtml: function() {
+        var items = this.items.slice(this.startIndex, this.endIndex);
+        var itemsHtml = this._createItemsHtml(items);
+        var cssText = this._createItemWrapperCssText(items.length, this.items.length);
+
+        return this._createDivHtml({
+            'class': CSS_CLASS_ITEM_WRAPPER,
+            style: cssText
+        }, itemsHtml);
+    },
+
+    /**
+     * Render contents.
+     * @param {?Number} scrollPosition - scroll position
+     * @private
+     */
+    _renderContents: function(scrollPosition) {
+        this.layout.innerHTML = this._createItemWrapperHtml();
+
+
+        if (tui.util.isExisty(scrollPosition)) {
+            this.layout.scrollTop = scrollPosition;
+        }
+    },
+
+    /**
+     * Handler for scroll event.
+     * @private
+     */
+    _onScroll: function() {
+        var scrollPosition = this.layout.scrollTop;
+        var scrollHeight = this.layout.scrollHeight - this.layout.offsetHeight;
+
+        this._updateIndexRange(scrollPosition);
+        this._renderContents();
+
+        this.fire(PUBLIC_EVENT_MAP.scroll, scrollPosition, scrollHeight);
+
+        if (scrollPosition === scrollHeight) {
+            this.fire(PUBLIC_EVENT_MAP.scrollBottom, scrollPosition, scrollHeight);
+        } else if (scrollPosition === 0) {
+            this.fire(PUBLIC_EVENT_MAP.scrollTop, scrollPosition, scrollHeight);
+        }
+    },
+
+    /**
+     * Attach event.
+     * @private
+     */
+    _attachEvent: function() {
+        eventListener.on(this.layout, 'scroll', this._onScroll, this);
+    },
+
+    /**
+     * Append items.
+     * @param {?Array.<String>} items - items
+     * @api
+     */
+    append: function(items) {
+        this.items = this.items.concat(items);
+        this._updateIndexRange(this.layout.scrollTop);
+        this._renderContents();
+    },
+
+    /**
+     * Prepend items.
+     * @param {?Array.<String>} items - items
+     * @api
+     */
+    prepend: function(items) {
+        var scrollPosition = this.layout.scrollTop + (items.length * this.itemHeight);
+
+        this.items = items.concat(this.items);
+        this._updateIndexRange(scrollPosition);
+        this._renderContents(scrollPosition);
+    },
+
+    /**
+     * Clear items.
+     * @api
+     */
+    clear: function() {
+        this.items = [];
+        this.layout.innerHTML = '';
+    },
+
+    /**
+     * Move scroll.
+     * @param {Number} scrollPosition - scroll position
+     * @api
+     */
+    moveScroll: function(scrollPosition) {
+        if (!tui.util.isNumber(scrollPosition)) {
+            throw new Error('This scroll position value is not number type');
+        }
+
+        this._updateIndexRange(scrollPosition);
+        this._renderContents(scrollPosition);
+    },
+
+    /**
+     * Get items.
+     * @returns {Array.<String>}
+     * @api
+     */
+    getItems: function() {
+        return this.items.slice();
+    },
+
+    /**
+     * Get scroll position value.
+     * @returns {Number}
+     * @api
+     */
+    getScrollPosition: function() {
+        return this.layout.scrollTop;
+    },
+
+    /**
+     * Destroy.
+     * @api
+     */
+    destroy: function() {
+        eventListener.off(this.layout, 'scroll', this._onScroll, this);
+        this.container.innerHTML = '';
+    }
+});
+
+tui.util.CustomEvents.mixin(VirtualScroll);
+
+/**
+ * NHN Entertainment Toast UI Chart.
+ * @namespace tui.chart
+ */
+tui.util.defineNamespace('tui.component');
+tui.component.VirtualScroll = VirtualScroll;
---
+++ test/js/virtualScroll.spec.js
@@ -0,0 +1,254 @@
+describe('tui.component.VirtualScroll', function() {
+    var virtualScroll;
+
+    beforeEach(function() {
+        var container = document.createElement('DIV');
+
+        document.body.appendChild(container);
+        virtualScroll = new tui.component.VirtualScroll(container, {});
+
+    });
+
+    describe('_updateIndexRange()', function() {
+        it('update index range', function() {
+            var scrollPosition = 120;
+
+            virtualScroll.items = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+            virtualScroll.itemHeight = 50;
+            virtualScroll.spareItemCount = 1;
+            virtualScroll.displayCount = 3;
+
+            virtualScroll._updateIndexRange(scrollPosition);
+
+            expect(virtualScroll.startIndex).toBe(1);
+            expect(virtualScroll.endIndex).toBe(6);
+        });
+
+        it('update index range, when scroll position is zero', function() {
+            var scrollPosition = 0;
+
+            virtualScroll.items = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+            virtualScroll.itemHeight = 50;
+            virtualScroll.spareItemCount = 1;
+            virtualScroll.displayCount = 3;
+
+            virtualScroll._updateIndexRange(scrollPosition);
+
+            expect(virtualScroll.startIndex).toBe(0);
+            expect(virtualScroll.endIndex).toBe(4);
+        });
+
+        it('update index range, when has not after spare', function() {
+            var scrollPosition = 320;
+
+            virtualScroll.items = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+            virtualScroll.itemHeight = 50;
+            virtualScroll.spareItemCount = 1;
+            virtualScroll.displayCount = 3;
+
+            virtualScroll._updateIndexRange(scrollPosition);
+
+            expect(virtualScroll.startIndex).toBe(5);
+            expect(virtualScroll.endIndex).toBe(7);
+        });
+    });
+
+    describe('_setDisplayCount()', function() {
+        it('set display count, when has displayCount option', function() {
+            var displayCount = 20;
+
+            virtualScroll._setDisplayCount(displayCount);
+
+            expect(virtualScroll.displayCount).toBe(20);
+        });
+
+        it('set display count, when has not displayCount option and has layoutHeight option', function() {
+            var displayCount;
+            var layoutHeight = 300;
+
+            virtualScroll.itemHeight = 50;
+            virtualScroll._setDisplayCount(displayCount, layoutHeight);
+
+            expect(virtualScroll.displayCount).toBe(6);
+        });
+
+        it('set display count, when has not both displayCount option and layoutHeight option', function() {
+            var displayCount, layoutHeight;
+
+            virtualScroll._setDisplayCount(displayCount, layoutHeight);
+
+            expect(virtualScroll.displayCount).toBe(10);
+        });
+    });
+
+    describe('_renderLayout()', function() {
+        it('if container is not exist, throw error message', function() {
+            var container;
+
+            expect(function() {
+                virtualScroll._renderLayout(container);
+            }).toThrowError('Not exist HTML container');
+        });
+
+        it('if container is not HTML element, throw error message', function() {
+            var container = 'container';
+
+            expect(function() {
+                virtualScroll._renderLayout(container);
+            }).toThrowError('This container is not HTML element');
+        });
+
+        it('render layout', function() {
+            var container = document.createElement('DIV');
+            var actual;
+
+            virtualScroll.layoutHeight = 700;
+            actual = virtualScroll._renderLayout(container);
+
+            expect(actual).toBe(container.firstChild);
+            expect(actual.className).toBe('tui-component-virtual-scroll');
+            expect(actual.style.height).toBe('700px');
+        });
+    });
+
+    describe('_createCssText()', function() {
+        it('create cssText by css map', function() {
+            var cssMap = {
+                height: 500,
+                'margin-top': 100,
+                'margin-bottom': 100
+            };
+            var actual = virtualScroll._createCssText(cssMap);
+
+            expect(actual).toBe('height:500px;margin-top:100px;margin-bottom:100px');
+        });
+    });
+
+    describe('_createDivHtml()', function() {
+        it('create div html', function() {
+            var attrMap = {
+                'class': 'className',
+                style: 'height:500px'
+            };
+            var innerHtml = 'inner html';
+            var actual = virtualScroll._createDivHtml(attrMap, innerHtml);
+
+            expect(actual).toBe('<div class="className" style="height:500px">inner html</div>');
+        });
+    });
+
+    describe('_createItemsHtml()', function() {
+        it('create items html', function() {
+            var items = ['A', 'B', 'C'];
+            var actual, expected;
+
+            virtualScroll.itemHeight = 50;
+
+            actual = virtualScroll._createItemsHtml(items);
+            expected = '<div class="tui-component-virtual-scroll-item" style="height:50px">A</div>' +
+                '<div class="tui-component-virtual-scroll-item" style="height:50px">B</div>' +
+                '<div class="tui-component-virtual-scroll-item" style="height:50px">C</div>';
+
+            expect(actual).toBe(expected);
+        });
+    });
+
+    describe('_createItemWrapperCssText()', function() {
+        it('create cssText for item wrapper element', function() {
+            var renderingItemCount = 10;
+            var itemCount = 20;
+            var actual;
+
+            virtualScroll.itemHeight = 50;
+            virtualScroll.startIndex = 5;
+            virtualScroll.endIndex = 15;
+
+            actual = virtualScroll._createItemWrapperCssText(renderingItemCount, itemCount);
+
+            expect(actual).toBe('height:500px;margin-top:250px;margin-bottom:250px');
+        });
+    });
+
+    describe('_createItemWrapperHtml()', function() {
+        it('create html for item wrapper element', function() {
+            var actual, expected;
+
+            virtualScroll.items = ['A', 'B', 'C', 'D', 'E'];
+            virtualScroll.itemHeight = 50;
+            virtualScroll.startIndex = 1;
+            virtualScroll.endIndex = 4;
+
+            actual = virtualScroll._createItemWrapperHtml();
+            expected = '<div class="tui-component-virtual-scroll-wrapper"' +
+                ' style="height:150px;margin-top:50px;margin-bottom:50px">' +
+                    '<div class="tui-component-virtual-scroll-item" style="height:50px">B</div>' +
+                    '<div class="tui-component-virtual-scroll-item" style="height:50px">C</div>' +
+                    '<div class="tui-component-virtual-scroll-item" style="height:50px">D</div>' +
+                '</div>';
+
+            expect(actual).toBe(expected);
+        });
+    });
+
+    describe('append()', function() {
+        it('append items', function() {
+            var wrapperElement;
+
+            virtualScroll.append(['A','B']);
+            virtualScroll.append(['C','D']);
+
+            wrapperElement = virtualScroll.layout.firstChild;
+            expect(virtualScroll.items).toEqual(['A','B','C','D']);
+            expect(wrapperElement.childNodes.length).toBe(4);
+            expect(wrapperElement.childNodes[0].innerHTML).toBe('A');
+            expect(wrapperElement.childNodes[1].innerHTML).toBe('B');
+            expect(wrapperElement.childNodes[2].innerHTML).toBe('C');
+            expect(wrapperElement.childNodes[3].innerHTML).toBe('D');
+        });
+    });
+
+    describe('prepend()', function() {
+        it('prepend items', function() {
+            var wrapperElement;
+
+            virtualScroll.prepend(['A','B']);
+            virtualScroll.prepend(['C','D']);
+
+            wrapperElement = virtualScroll.layout.firstChild;
+            expect(virtualScroll.items).toEqual(['C','D','A','B']);
+            expect(wrapperElement.childNodes.length).toBe(4);
+            expect(wrapperElement.childNodes[0].innerHTML).toBe('C');
+            expect(wrapperElement.childNodes[1].innerHTML).toBe('D');
+            expect(wrapperElement.childNodes[2].innerHTML).toBe('A');
+            expect(wrapperElement.childNodes[3].innerHTML).toBe('B');
+        });
+    });
+
+    describe('clear()', function() {
+        it('clear items', function() {
+            var wrapperElement;
+
+            virtualScroll.append(['A','B']);
+
+            wrapperElement = virtualScroll.layout.firstChild;
+            expect(virtualScroll.items).toEqual(['A','B']);
+            expect(wrapperElement.childNodes.length).toBe(2);
+            expect(wrapperElement.childNodes[0].innerHTML).toBe('A');
+            expect(wrapperElement.childNodes[1].innerHTML).toBe('B');
+
+            virtualScroll.clear();
+            expect(virtualScroll.items.length).toBe(0);
+            expect(virtualScroll.layout.innerHTML).toBe('');
+        });
+    });
+
+    describe('moveScroll()', function() {
+        it('if scroll position value is not number type, throw error message', function() {
+            var scrollPosition = '700';
+
+            expect(function() {
+                virtualScroll.moveScroll(scrollPosition);
+            }).toThrowError('This scroll position value is not number type');
+        });
+    })
+});
